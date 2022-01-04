d453870c1477a531b5aaed4c77f2f9edb3432b56
minorusso <gumbomb1@gmail.com>
2022-01-04 14:34:25 +0900

    Add task create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,23 @@
 class TasksController < ApplicationController
   def index
-  end
-
-  def show
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def show
+
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get 'tasks/show'
-  get 'tasks/new'
+  root to: 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
