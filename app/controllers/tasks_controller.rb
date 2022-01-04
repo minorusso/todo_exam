a6f3ea386f5fb5e8fac0f6fc3b17ae933edb4382
minorusso <gumbomb1@gmail.com>
2022-01-04 14:55:02 +0900

    Add task CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -6,18 +7,41 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-
+  
   def create
-    Task.create(task_params)
-    redirect_to tasks_path
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "ブログ作成しました!"
+    else
+      render :new
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました!"
+    else
+      render :edit
+    end
   end
 
-  def show
-
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました!"
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
